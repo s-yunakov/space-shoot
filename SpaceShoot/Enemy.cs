c2843030ede29e6dd2973c0ae00917d7934bc4a9
Stanislav Yunakov - STUDENT <G00471112@atu.ie>
2026-09-12 22:50:38 +0100

    feat: add basic enemy entity

---
+++ SpaceShoot/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceShoot
+{
+    public class Enemy
+    {
+        public double X { get; private set; }
+        public double Y { get; private set; }
+        public double Size { get; private set; } = 30;
+        public BoxView Visual { get; private set; }
+
+        public Enemy(double x, double y)
+        {
+            X = x;
+            Y = y;
+
+            Visual = new BoxView
+            {
+                Color = Colors.Red,
+                WidthRequest = Size,
+                HeightRequest = Size,
+                CornerRadius = Size / 2
+            };
+        }
+    }
+}
