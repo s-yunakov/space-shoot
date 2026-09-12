a4e9e4bb9cce021301ea15a357763b6195b27068
Stanislav Yunakov - STUDENT <G00471112@atu.ie>
2026-09-13 00:01:23 +0100

    feat: add enemy movement

--- SpaceShoot/Enemy.cs
+++ SpaceShoot/Enemy.cs
@@ -13,6 +13,10 @@ namespace SpaceShoot
         public double Size { get; private set; } = 30;
         public BoxView Visual { get; private set; }
 
+        private double velocityX;
+        private double velocityY;
+        private double speed = 2.0;
+
         public Enemy(double x, double y)
         {
             X = x;
@@ -25,6 +29,12 @@ namespace SpaceShoot
                 HeightRequest = Size,
                 CornerRadius = Size / 2
             };
+        }
+
+        public void Update(double screenWidth, double screenHeight)
+        {
+            X += velocityX;
+            Y += velocityY;
         }
     }
 }
