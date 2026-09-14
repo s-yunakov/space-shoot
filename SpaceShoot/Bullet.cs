493431c218910479d4bf7471ac1a53b36ef1667b
Stanislav Yunakov - STUDENT <G00471112@atu.ie>
2026-09-14 19:35:21 +0100

    feat: add basic bullet entity

---
+++ SpaceShoot/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceShoot
+{
+    public class Bullet
+    {
+        public double X { get; private set; }
+        public double Y { get; private set; }
+        public BoxView Visual { get; private set; }
+
+        public Bullet(double x, double y, double directionX, double directionY)
+        {
+            X = x;
+            Y = y;
+
+            Visual = new BoxView
+            {
+                WidthRequest = 6,
+                HeightRequest = 20,
+                Color = Colors.Yellow,
+                CornerRadius = 3
+            };
+        }
+    }
+}
