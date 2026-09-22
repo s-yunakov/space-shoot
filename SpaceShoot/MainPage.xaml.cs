1fcbac33ee8b291a9b2e1945a8d3c90e36490a69
Stanislav Yunakov - STUDENT <G00471112@atu.ie>
2026-09-22 20:58:30 +0100

    feat: update and remove bullets

--- SpaceShoot/MainPage.xaml.cs
+++ SpaceShoot/MainPage.xaml.cs
@@ -106,6 +106,24 @@
                 return;
 
             // Update all bullets
+            for (int i = bullets.Count - 1; i >= 0; i--)
+            {
+                bullets[i].Update();
+
+                AbsoluteLayout.SetLayoutBounds(
+                    bullets[i].Visual,
+                    new Rect(
+                        bullets[i].X - 3,
+                        bullets[i].Y - 10,
+                        6,
+                        20));
+
+                if (!bullets[i].IsOnScreen(canvasWidth, canvasHeight))
+                {
+                    GameCanvas.Children.Remove(bullets[i].Visual);
+                    bullets.RemoveAt(i);
+                }
+            }
 
             // Update all enemies
             for (int i = enemies.Count - 1; i >= 0; i--)
